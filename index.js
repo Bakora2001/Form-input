c2dc4e5140a3badee5a61f83ea5cccd4edf32132
Maxwel <maxwellbakora@gmail.com>
2024-04-03 08:56:26 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,17 @@
+document.getElementById("gradeForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    const score = parseInt(document.getElementById("score").value);
+    let grade = '';
+    if (score >= 90) {
+      grade = 'A';
+    } else if (score >= 80) {
+      grade = 'B';
+    } else if (score >= 70) {
+      grade = 'C';
+    } else if (score >= 60) {
+      grade = 'D';
+    } else {
+      grade = 'E';
+    }
+    document.getElementById("output").textContent = `Your grade is: ${grade}`;
+  });
